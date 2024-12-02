5d9247ac6a1f0658c64a0403b7545b5c5a52269c
Martinillan <chechu1893@hotmail.com>
2024-12-02 10:24:57 +0100

    Update app.js

--- app.js
+++ app.js
@@ -2,14 +2,13 @@ const express = require('express');
 const { Pool } = require('pg');
 
 const app = express();
-const PORT = 3000; // Puerto donde correrá la app
-
+const PORT = 3000; 
 const pool = new Pool({
-    user: 'postgres',          // Usuario de PostgreSQL
-    host: 'localhost',           // Host de la base de datos
-    database: 'empresa',     // Nombre de la base de datos
-    password: 'Martin76.',   // Contraseña de PostgreSQL
-    port: 5432,                  // Puerto por defecto de PostgreSQL
+    user: 'postgres',         
+    host: 'localhost',          
+    database: 'empresa',     
+    password: 'Martin76.',   
+    port: 5432,                  
 });
 
 pool.connect((err) => {
@@ -22,8 +21,7 @@ pool.connect((err) => {
 
   app.get('/empleados', async (req, res) => {
     try {
-      const result = await pool.query('SELECT * FROM empleados'); // Cambia "empleados" por el nombre exacto de tu tabla
-      res.json(result.rows); // Devolver los datos como JSON
+      const result = await pool.query('SELECT * FROM empleados'); 
     } catch (err) {
       console.error('Error al ejecutar la consulta:', err);
       res.status(500).send('Error en el servidor');
